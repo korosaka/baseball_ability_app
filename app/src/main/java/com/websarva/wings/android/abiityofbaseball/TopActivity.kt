a1399755ab122695eb1c241e6b3f05dd6706815a
github koro <koro1008.it@gmail.com>
2019-01-13 16:14:46 +0900

    プロジェクト作成！

---
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/TopActivity.kt
@@ -0,0 +1,12 @@
+package com.websarva.wings.android.abiityofbaseball
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top)
+    }
+}
