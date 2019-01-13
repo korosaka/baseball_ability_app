6685fa6611ba1e8d331bfb74e8459a2f7e5b386f
github koro <koro1008.it@gmail.com>
2019-01-13 19:36:55 +0900

    選手作成画面作成し、そこまでintentで画面遷移機能実装

--- app/src/main/java/com/websarva/wings/android/abiityofbaseball/SelectOptionActivity.kt
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/SelectOptionActivity.kt
@@ -1,12 +1,22 @@
 package com.websarva.wings.android.abiityofbaseball
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class SelectOptionActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_option)
+    }
+
+    // 選手をつくるボタンクリックで画面遷移
+    fun onClickStartMaking(view : View){
+
+        val intent = Intent(this,SelectPlayerTypeActivity::class.java)
+        startActivity(intent)
+
     }
 }
--- app/src/main/java/com/websarva/wings/android/abiityofbaseball/SelectPlayerTypeActivity.kt
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/SelectPlayerTypeActivity.kt
@@ -1,7 +1,9 @@
 package com.websarva.wings.android.abiityofbaseball
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class SelectPlayerTypeActivity : AppCompatActivity(){
 
@@ -9,5 +11,14 @@ class SelectPlayerTypeActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_player_type)
     }
+
+    // 選手をつくるボタンクリックで画面遷移
+    fun onClickSelectFielder(view : View){
+
+        val intent = Intent(this,PlayerMakingActivity::class.java)
+        startActivity(intent)
+
+    }
+
 
 }
