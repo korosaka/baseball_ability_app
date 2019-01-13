03dcbebf0474b2a9249ef7e43bc4ca9eb3ce5257
github koro <koro1008.it@gmail.com>
2019-01-13 17:26:19 +0900

    画面遷移機能実装

--- app/src/main/java/com/websarva/wings/android/abiityofbaseball/TopActivity.kt
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/TopActivity.kt
@@ -1,12 +1,22 @@
 package com.websarva.wings.android.abiityofbaseball
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class TopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top)
+    }
+
+    // はじめるボタンクリックで画面遷移
+    fun onClickAppStart(view : View){
+
+        val intent = Intent(this,SelectTypeActivity::class.java)
+        startActivity(intent)
+
     }
 }
