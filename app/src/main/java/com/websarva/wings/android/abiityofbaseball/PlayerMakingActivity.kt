f887d7795dd8dfb2df6f9589559aa6ff6d747459
github koro <koro1008.it@gmail.com>
2019-01-14 21:02:58 +0900

    fragmentまでいくも、エラー

--- app/src/main/java/com/websarva/wings/android/abiityofbaseball/PlayerMakingActivity.kt
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/PlayerMakingActivity.kt
@@ -8,6 +8,13 @@ class PlayerMakingActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_making)
+
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frame_for_fragment,QuestionOfAppearanceFragment.newInstance("a","b"))
+        transaction.commit()
+
+
     }
 
 }
--- app/src/main/java/com/websarva/wings/android/abiityofbaseball/SelectPlayerTypeActivity.kt
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/SelectPlayerTypeActivity.kt
@@ -4,19 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
 
 class SelectPlayerTypeActivity : AppCompatActivity(){
+
+    companion object {
+        const val EXTRA_TEXTDATA = "playerName"
+    }
+
+    private val editText = findViewById<EditText>(R.id.player_name) as EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_player_type)
+
     }
 
     // 選手をつくるボタンクリックで画面遷移
     fun onClickSelectFielder(view : View){
 
-        val intent = Intent(this,PlayerMakingActivity::class.java)
-        startActivity(intent)
+        if(editText != null){
+
+            val playerName = editText.text.toString()
+
+            val intent = Intent(this,PlayerMakingActivity::class.java)
+
+            intent.putExtra(EXTRA_TEXTDATA,playerName)
+
+            startActivity(intent)
+
+        }
+
 
     }
 
