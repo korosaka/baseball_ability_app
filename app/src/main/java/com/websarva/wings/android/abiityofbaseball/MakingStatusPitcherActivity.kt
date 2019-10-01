2d5eba83e2fe78607306e2ec6050c7054ddbb4bd
koro saka <koro.nn99@gmail.com>
2019-10-01 18:05:08 +0900

    変化球までレイアウト表示

--- app/src/main/java/com/websarva/wings/android/abiityofbaseball/MakingStatusPitcherActivity.kt
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/MakingStatusPitcherActivity.kt
@@ -2,12 +2,22 @@ package com.websarva.wings.android.abiityofbaseball
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_making_status.*
 
 class MakingStatusPitcherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_making_status_pitcher)
+
+
+        // 広告処理
+        MobileAds.initialize(this,"ca-app-pub-6298264304843789~4492140864")
+        val adRequest = AdRequest.Builder().build()
+        adView_status.loadAd(adRequest)
+
     }
 
 }
