04a1fa43bca53af8739d7e49c7d26a4cd36eecb2
koro saka <koro.nn99@gmail.com>
2019-10-01 16:45:04 +0900

    投手画面追加

---
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/MakingStatusPitcherActivity.kt
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.abiityofbaseball
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MakingStatusPitcherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_making_status_pitcher)
+    }
+
+}
