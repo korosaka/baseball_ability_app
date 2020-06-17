aef3652e07a42c2a6091b0248a8663c086608762
koro saka <koro.nn99@gmail.com>
2020-06-17 01:59:26 -0700

    refactor how to write

--- app/src/main/java/com/websarva/wings/android/abiityofbaseball/MakingStatusActivity.kt
+++ app/src/main/java/com/websarva/wings/android/abiityofbaseball/MakingStatusActivity.kt
@@ -99,13 +99,18 @@ class MakingStatusActivity : BaseBannerActivity() {
     private fun calcRecord(player: PlayerClass) {
 
         var ave = 85 + (player.contactAbility * 4.5).toInt() + (player.powerAbility * 0.45).toInt() + (player.speedAbility * 0.15).toInt()
-        if (ave < 150) {
-            ave = 120 + (ave * 0.3).toInt()
-        } else if (ave < 200) {
-            ave = 150 + (ave * 0.3).toInt()
-        } else if (ave > 300) {
-            ave = ((ave - 300) * 0.5).toInt() + 300
+        when {
+            ave < 150 -> {
+                ave = 120 + (ave * 0.3).toInt()
+            }
+            ave < 200 -> {
+                ave = 150 + (ave * 0.3).toInt()
+            }
+            ave > 300 -> {
+                ave = ((ave - 300) * 0.5).toInt() + 300
+            }
         }
+
         ave = when (player.ballisticAbility) {
             1 -> ave
             2 -> (ave * 0.99).toInt()
@@ -125,49 +130,48 @@ class MakingStatusActivity : BaseBannerActivity() {
             else -> hr
         }
         var rbi = (((player.contactAbility * 0.5) + (player.powerAbility * 1.0)) * player.chance).toInt()
-        if (rbi < 0) {
-            rbi = 1
-        } else if (rbi < 30) {
-            rbi = (rbi * 0.2).toInt()
-        } else if (rbi < 50) {
-            rbi = (rbi * 0.35).toInt()
-        } else if (rbi < 60) {
-            rbi = (rbi * 0.6).toInt()
-        } else if (rbi < 70) {
-            rbi = (rbi * 0.75).toInt()
-        } else if (rbi < 80) {
-            rbi = (rbi * 0.9).toInt()
+        when {
+            rbi < 0 -> {
+                rbi = 1
+            }
+            rbi < 30 -> {
+                rbi = (rbi * 0.2).toInt()
+            }
+            rbi < 50 -> {
+                rbi = (rbi * 0.35).toInt()
+            }
+            rbi < 60 -> {
+                rbi = (rbi * 0.6).toInt()
+            }
+            rbi < 70 -> {
+                rbi = (rbi * 0.75).toInt()
+            }
+            rbi < 80 -> {
+                rbi = (rbi * 0.9).toInt()
+            }
         }
-        if (rbi < hr) {
-            rbi = hr
-        }
+        if (rbi < hr) rbi = hr
         var sb = (player.speedAbility * 0.8).toInt() - 25
-        if (sb < -10) {
-            sb = (player.speedAbility * 0.1).toInt()
-        } else if (sb < 5) {
-            sb = (player.speedAbility * 0.1).toInt() + 1
-        } else if (sb > 30) {
-            sb = (sb * 1.1).toInt()
+        when {
+            sb < -10 -> {
+                sb = (player.speedAbility * 0.1).toInt()
+            }
+            sb < 5 -> {
+                sb = (player.speedAbility * 0.1).toInt() + 1
+            }
+            sb > 30 -> {
+                sb = (sb * 1.1).toInt()
+            }
+
         }
 
 
-        // TODO refactor ?
-        val ave_dis = findViewById<TextView>(R.id.average_display)
-        val hr_dis = findViewById<TextView>(R.id.homer_display)
-        val rbi_dis = findViewById<TextView>(R.id.rbi_display)
-        val sb_dis = findViewById<TextView>(R.id.steel_display)
-
-        // 打率表示
-        var ave_string: String? = null
-        if (ave < 100) {
-            ave_string = "." + "0" + Integer.toString(ave)
-        } else {
-            ave_string = "." + Integer.toString(ave)
-        }
-        ave_dis.text = ave_string
-        hr_dis.text = Integer.toString(hr)
-        rbi_dis.text = Integer.toString(rbi)
-        sb_dis.text = Integer.toString(sb)
+        var aveString = ".$ave"
+        if (ave < 100) aveString = ".0$ave"
+        average_display.text = aveString
+        homer_display.text = hr.toString()
+        rbi_display.text = rbi.toString()
+        steel_display.text = sb.toString()
 
         calcSalary(ave, hr, rbi, sb, player)
 
@@ -272,19 +276,19 @@ class MakingStatusActivity : BaseBannerActivity() {
 
         if (salary < 10000) {
             oku_unit.visibility = View.GONE
-            man_number.text = Integer.toString(salary)
+            man_number.text = salary.toString()
         } else {
             var okuNumber = 0
             while (salary >= 10000) {
                 salary -= 10000
                 okuNumber++
             }
-            oku_number.text = Integer.toString(okuNumber)
+            oku_number.text = okuNumber.toString()
 
             if (salary == 0) {
                 man_unit.visibility = View.GONE
             } else {
-                man_number.text = Integer.toString(salary)
+                man_number.text = salary.toString()
             }
         }
 
